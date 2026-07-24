7c5f19aef3e1113855797288ffcea55963ce8702
fazli03 <siputkencang3@gmail.com>
2026-07-24 14:32:33 +0700

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
 export default function Footer() {
   return (
     <footer>
-      <span className="footer-copy">
-        © 2026 Fazli Maulana — All Rights Reserved
-      </span>
+      <span className="footer-copy">© 2026 Fozeals — All Rights Reserved</span>
     </footer>
   );
 }
+  
